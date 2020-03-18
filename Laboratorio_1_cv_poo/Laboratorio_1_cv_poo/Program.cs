e367c029191342eb04b830bddd2efa1b7717b997
Camila Villalobos <camilavillalobos@MacBook-Air-de-Camila-6.local>
2020-03-18 16:51:25 -0300

    Primera linea laboratorio

---
+++ Laboratorio_1_cv_poo/Laboratorio_1_cv_poo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Laboratorio_1_cv_poo
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
